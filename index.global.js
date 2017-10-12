94cea4c1fec1b8f93aee9dfde308d38ebe60cb1e
quietcoder <1642965215@qq.com>
2017-10-12 20:06:31 +0800

    add: global module

---
+++ index.global.js
@@ -0,0 +1,8 @@
+window.openNewPage = function (url) {
+  var form = document.createElement('form')
+  form.action = url
+  form.target = '_blank'
+  form.method = 'POST'
+  document.body.appendChild(form)
+  form.submit()
+}
